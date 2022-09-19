4578cfd61fa5d20c0f4a9c051e7963ef74872cd2
mai1015 <i@mai1015.com>
2022-09-19 05:03:00 +0000

    setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,51 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config"
+import "hardhat-deploy"
 import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const KOVAN_RPC_URL =
+    process.env.KOVAN_RPC_URL ||
+    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+const PRIVATE_KEY =
+    process.env.PRIVATE_KEY ||
+    "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      // gasPrice: 130000000000,
+    },
+    kovan: {
+      url: KOVAN_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 42,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
   gasReporter: {
-    enabled: true
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    user: {
+      default: 1,
+    }
   }
 };
 
